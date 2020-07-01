366ab90618b17db8e9c333946315059b85deb2fd
caoyufei <atcaoyufei@gmail.com>
2020-07-01 13:51:03 +0800

    'huawei'

--- huawei.py
+++ huawei.py
@@ -1,10 +1,11 @@
+import argparse
 import asyncio
 
 from pyppeteer import launch
 
 
-async def main():
-    browser = await launch(ignorehttpserrrors=True, headless=False,
+async def main(username, password):
+    browser = await launch(ignorehttpserrrors=True, headless=True,
                            args=['--disable-infobars', '--no-sandbox', '--start-maximized'])
     page = await browser.newPage()
     await page.setViewport({'width': 1200, 'height': 768})
@@ -13,59 +14,48 @@ async def main():
     await page.waitForSelector('#personalAccountInputId .tiny-input-text', {'visible': True})
 
     await page.evaluate('''() =>{ Object.defineProperties(navigator,{ webdriver:{ get: () => false } }) }''')
-    await page.type('#personalAccountInputId .tiny-input-text', 'caoyufei')
-    await page.type('#personalPasswordInputId .tiny-input-text', 'hack3321')
+    await page.type('#personalAccountInputId .tiny-input-text', username)
+    await page.type('#personalPasswordInputId .tiny-input-text', password)
     await page.click('#btn_submit')
     await asyncio.sleep(5)
 
-    try:
-        nav = (await page.xpath('//ul[@class="devui-nav devui-nav-options"]'))[0]
-        elements = await nav.querySelectorAll('li')
-        for element in elements:
-            await element.click()
+    page_url = page.url
+    print(page_url)
+    if page_url.find('mobile') != -1:
+        await page.waitForSelector('.mobile-loading-btn-body', {'visible': True})
+        sign_txt = str(await page.Jeval('.mobile-loading-btn-body', 'el => el.textContent')).strip()
+        credit = str(await page.Jeval('.count', 'el => el.textContent')).replace('码豆', '').strip()
+        print(f'签到前码豆: {credit}')
 
-            feedback_item_list = await page.xpath('//div[starts-with(@id, "experience-missions")]')
-            for item in feedback_item_list:
-                complate_img = await item.querySelector('.complate-img')
-                if complate_img:
-                    continue
+        if sign_txt.find('今日已签到') == -1:
+            await page.click('.mobile-loading-btn-body')
+            await asyncio.sleep(3)
+            new_credit = str(await page.Jeval('.count', 'el => el.textContent')).replace('码豆', '').strip()
+            print(f'签到后码豆: {new_credit}')
 
-                await item.click()
-                await asyncio.sleep(2)
-                await page.click('.devui-btn.devui-btn-stress.devui-btn-md')
+    else:
+        await page.waitForSelector('#homeheader-signin, #homeheader-signined', {'visible': True})
+        info = await page.Jeval('#homeheader-signin span.button-content, #homeheader-signined  span.button-content',
+                                'el => el.textContent')
+        sign_txt = str(info).strip()
+        credit = str(await page.Jeval('#homeheader-coins', 'el => el.textContent')).strip()
+        print(f'签到前码豆: {credit}')
 
-                await asyncio.sleep(5)
-                pages = (await browser.pages())
-                print(pages[-1].url)
-                # for i, p in enumerate(pages):
-                #     print(i, p.url)
-                # print(page2.url)
-                # await page2.click('.devui-btn.devui-btn-primary.devui-btn-md')
+        if sign_txt.find('已签到') == -1:
+            await page.click('#homeheader-signin')
+            await asyncio.sleep(3)
+            new_credit = str(await page.Jeval('#homeheader-coins', 'el => el.textContent')).strip()
+            print(f'签到后码豆: {new_credit}')
 
-            break
-    except Exception:
-        pass
-
-    # print(await (await nav.getProperty('textContent')).jsonValue())
-
-    # page_url = page.url
-    # print(page_url)
-    # if page_url.find('mobile') != -1:
-    #     await page.waitForSelector('.mobile-loading-btn-body', {'visible': True})
-    #     print(await page.Jeval('.count', 'el => el.textContent'))
-    #     await page.click('.mobile-loading-btn-body')
-    #     await asyncio.sleep(3)
-    #     print(await page.Jeval('.count', 'el => el.textContent'))
-    # else:
-    #     await page.waitForSelector('#homeheader-signin', {'visible': True})
-    #     print(await page.Jeval('#homeheader-coins', 'el => el.textContent'))
-    #     await page.click('#homeheader-signin')
-    #     await asyncio.sleep(3)
-    #     print(await page.Jeval('#homeheader-coins', 'el => el.textContent'))
-
-    await asyncio.sleep(50)
+    await asyncio.sleep(3)
     await page.close()
     await browser.close()
 
 
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    args = parser.parse_args()
+    params = vars(args)
+    asyncio.get_event_loop().run_until_complete(main(params.get('username'), params.get('password')))
