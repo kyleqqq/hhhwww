262f953ffc7125c493ceb988ec0923018aab4477
caoyufei <atcaoyufei@gmail.com>
2020-07-07 20:37:45 +0800

    gandi

--- gandi.py
+++ gandi.py
@@ -11,38 +11,40 @@ from pyppeteer import launch
 async def main(username, password):
     browser = await launch(ignorehttpserrrors=True, headless=True,
                            args=['--disable-infobars', '--no-sandbox', '--window-size=1920,1080'])
-    page = await browser.newPage()
-    print(time.strftime('%Y-%m-%d %H:%M:%S'))
-    try:
-        email_suffix = ['demo666.cn', 'zzcworld.com']
-        name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-        email = f'{name}@{random.choice(email_suffix)}'
 
-        # await page.setViewport({'width': 1920, 'height': 1080})
-        await page.goto('https://20.gandi.net/zh-hant/lottery-wheel/', {'waitUntil': 'load'})
+    for i in range(100):
+        print(time.strftime('%Y-%m-%d %H:%M:%S'))
+        page = await browser.newPage()
 
-        await page.waitForSelector('#email', {'visible': True})
+        try:
+            email_suffix = ['demo666.cn', 'zzcworld.com']
+            name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+            email = f'{name}@{random.choice(email_suffix)}'
 
-        await page.type('#email', email)
-        await asyncio.sleep(1)
+            # await page.setViewport({'width': 1920, 'height': 1080})
+            await page.goto('https://20.gandi.net/zh-hant/lottery-wheel/', {'waitUntil': 'load'})
 
-        await page.evaluate(
-            '''() =>{ document.getElementById('reglement').checked=true }''')
+            await page.waitForSelector('#email', {'visible': True})
 
-        await page.evaluate(
-            '''() =>{ document.getElementById('newsLetter').checked=true }''')
+            await page.type('#email', email)
+            await asyncio.sleep(1)
 
-        await asyncio.sleep(1)
+            await page.evaluate(
+                '''() =>{ document.getElementById('reglement').checked=true }''')
 
-        await page.click('#gandi20Years-wheel-form-submit')
+            await page.evaluate(
+                '''() =>{ document.getElementById('newsLetter').checked=true }''')
 
-        await asyncio.sleep(5)
+            await asyncio.sleep(1)
 
-    except Exception as e:
-        logging.exception(e)
-    finally:
-        await page.close()
-        await browser.close()
+            await page.click('#gandi20Years-wheel-form-submit')
+
+            await asyncio.sleep(5)
+            await page.close()
+        except Exception as e:
+            print(e)
+            await page.close()
+        await asyncio.sleep(10)
 
 
 if __name__ == '__main__':
