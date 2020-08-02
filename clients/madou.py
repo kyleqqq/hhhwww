3825ffaddd4553995483ec4146d6afca21103829
zou hua <zouhua@zoudeMBP.lan>
2020-08-02 18:10:15 +0800

    madou

--- clients/madou.py
+++ clients/madou.py
@@ -1,4 +1,7 @@
 import asyncio
+import os
+import time
+from pathlib import Path
 
 from libs.base import BaseClient
 
@@ -19,33 +22,71 @@ class MaDou(BaseClient):
         self.logger.info(self.page.url)
         # await self.page.goto(self.url, {'waitUntil': 'load'})
 
-        await self.page.waitForSelector('#daily-mission-wrapper', {'visible': True})
-
-        await self.page.click(
-            '#daily-mission-wrapper > div.ng-star-inserted:nth-child(1) ul li.ng-star-inserted:nth-child(2)')
-        await asyncio.sleep(1)
-
         # element = await self.page.querySelector(
         #     '#daily-mission-wrapper > div.ng-star-inserted:nth-child(1) .devui-tab-content li:nth-child(2)')
         # self.logger.info(element)
         # self.logger.info(await (await element.getProperty('textContent')).jsonValue())
 
+        # await self.open_ide_task()
+        # await self.open_code_task()
+        await self.push_code_task()
+
+        # print(len(page_list))
+        # for page in page_list:
+        #     print(page.url)
+
+        await asyncio.sleep(2)
+
+    async def get_new_page(self, a=2, b=1):
+        await self.page.waitForSelector('#daily-mission-wrapper', {'visible': True})
+
         await self.page.click(
-            '#daily-mission-wrapper div.ng-star-inserted:nth-child(1) .devui-tab-content #experience-missions-1')
+            f'#daily-mission-wrapper > div.ng-star-inserted:nth-child(1) ul li.ng-star-inserted:nth-child({a})')
+        await asyncio.sleep(1)
+
+        await self.page.click(
+            f'#daily-mission-wrapper div.ng-star-inserted:nth-child(1) .devui-tab-content #experience-missions-{b}')
         await asyncio.sleep(1)
 
         await self.page.click('.modal.in .button-content')
         await asyncio.sleep(5)
 
         page_list = await self.browser.pages()
-        new_page = page_list[-1]
+        return page_list[-1]
+
+    async def open_code_task(self):
+        new_page = await self.get_new_page()
         await new_page.waitForSelector('.btn_cloudide', {'visible': True})
         await new_page.click('.btn_cloudide')
-
         await asyncio.sleep(20)
+        await new_page.close()
 
-        print(len(page_list))
-        for page in page_list:
-            print(page.url)
+    async def open_ide_task(self):
+        new_page = await self.get_new_page(3, 0)
+        await new_page.waitForSelector('.trial-stack-info', {'visible': True})
+        await new_page.click('.trial-stack-info .stack-content .stack-position .devui-btn')
+        await asyncio.sleep(20)
+        await new_page.close()
 
-        await asyncio.sleep(10)
+    async def push_code_task(self):
+        await self.get_new_page(2, 2)
+
+        now_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        cmd = [
+            'cd /tmp',
+            'git config --global user.name "caoyufei" && git config --global user.email "atcaoyufei@gmail.com"',
+            'git clone https://codehub.devcloud.huaweicloud.com/python00020/crawler.git',
+            'cd /tmp/crawler',
+            f'echo "{now_time}" >> time.txt',
+            "git commit -am 'time'",
+            "git push origin master"
+        ]
+        id_rsa = os.environ.get('id_rsa')
+        ssh_dir = Path.home() / '.ssh'
+        ssh_dir.mkdir()
+        file = ssh_dir / 'id_rsa'
+        file.write_text(id_rsa)
+
+        os.system(' && '.join(cmd))
+
+        await asyncio.sleep(3)
