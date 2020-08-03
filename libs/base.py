5dd168c9455ed3d67e78d124ffb607c989f9eb1b
caoyufei <atcaoyufei@gmail.com>
2020-08-03 09:19:29 +0800

    madou

--- libs/base.py
+++ libs/base.py
@@ -17,6 +17,8 @@ class BaseClient:
         self.browser = await launch(ignorehttpserrrors=True, headless=kwargs.get('headless', True),
                                     args=['--disable-infobars', '--no-sandbox', '--start-maximized'])
         self.page = await self.browser.newPage()
+        await self.page.setUserAgent(
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36')
         await self.page.setViewport({'width': 1200, 'height': 768})
         await self.page.goto(self.url, {'waitUntil': 'load'})
         try:
