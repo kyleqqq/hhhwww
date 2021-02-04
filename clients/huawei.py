40aaf0f844570f32914108a29ce412a3b41cd166
caoyufei <atcaoyufei@gmail.com>
2021-02-04 20:46:00 +0800

    1.1

--- clients/huawei.py
+++ clients/huawei.py
@@ -21,16 +21,16 @@ class HuaWei(BaseHuaWei):
         utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
         h = int(utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%H'))
 
-        await self.check_project()
-
-        await self.sign_task()
-        await self.start()
+        # await self.check_project()
+        #
+        # await self.sign_task()
+        # await self.start()
 
         if h >= 11:
             await self.delete_project()
-            await self.delete_function()
-            await self.delete_api()
-            await self.delete_api_group()
+            # await self.delete_function()
+            # await self.delete_api()
+            # await self.delete_api_group()
 
         # await self.init_account()
 
